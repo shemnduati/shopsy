dd1fb6281f1a9a5d20b7b97c37328a93ada3b0a5
shemnduati <nduatishem@gmail.com>
2024-10-31 10:09:20 +0300

    pushing small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Products from './components/Products/Products';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <Products />
     </>
   )
 }
