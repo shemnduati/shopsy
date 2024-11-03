df54d92300f0d75de44d8c0b4cda9b162dbe717b
shemnduati <nduatishem@gmail.com>
2024-11-03 06:14:36 +0300

    completed working with the bannner secrtion

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Img1 from "../../assets/shirt/shirt.png";
 import Img2 from "../../assets/shirt/shirt2.png";
 import Img3 from "../../assets/shirt/shirt3.png";
+import { FaStar } from 'react-icons/fa6';
 
 const ProductsData = [
     {
@@ -53,6 +54,7 @@ const TopProducts = () => {
                 {
                     ProductsData.map((data) => (
                         <div 
+                            data-aos="zoom-in"
                             className='rounded-2xl bg-white dark:bg-gray-800
                                      hover:bg-black/80 dark:hover:bg-primary
                                      hover:text-white
@@ -66,9 +68,23 @@ const TopProducts = () => {
                                                 group-hover:scale-105 duration-300 drop-shadow-md'
                                 />
                             </div>
-                            <div>
-                                <h3 className='font-semibold'>{data.title}</h3>
-                                <p className='text-sm text-gray-600'>{data.color}</p>
+                            {/* details section  */}
+                            <div className='p-4 text-center'>
+                                {/* star rating */}
+                                <div className='w-full flex item-center justify-center gap-1'>
+                                    <FaStar className='text-yellow-500' />
+                                    <FaStar className='text-yellow-500' />
+                                    <FaStar className='text-yellow-500' />
+                                    <FaStar className='text-yellow-500' />
+                                </div>
+                                <h1 className='text-xl font-bold'>{data.title}</h1>
+                                <p className='text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2'>
+                                    {data.description}
+                                </p>
+                                <button className='bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4
+                                                    group-hover:bg-white group-hover:text-primary'>
+                                    Order Now
+                                </button>
                             </div>
                         </div>
                     ))
